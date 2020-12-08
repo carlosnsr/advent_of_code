2e147753a83ca45de9ff005e0f7cf03403d4fdc6
Carlos Rodrigues <carlos.all.mail@gmail.com>
2020-12-07 21:15:23 -0700

    Changed it to be non-destructive

--- 2020/1/main.rb
+++ 2020/1/main.rb
@@ -1,24 +1,25 @@
 TARGET_SUM = 2020
-MID_POINT = TARGET_SUM / 2
+
+def find_sum_pair(numbers, target_sum)
+  mid_point = target_sum / 2
+  numbers.each do |lowest|
+    # once we hit numbers higher than mid_point there are no more possible solutions
+    break if lowest >= mid_point
+    difference = target_sum - lowest
+    found = numbers.bsearch { |num| difference - num }
+    return [lowest, found] unless found.nil?
+  end
+  []
+end
 
 def main
   numbers = []
   ARGF.each { |line| numbers << line.strip.to_i }
   numbers.sort!
 
-  highest = numbers.pop
-  # once we hit numbers less than MID_POINT there are no more possible solutions
-  while highest >= MID_POINT do
-    difference = TARGET_SUM - highest
-    found = numbers.bsearch { |num| difference - num }
-    unless found.nil?
-      puts "first: #{highest}"
-      puts "second: #{found}"
-      puts "product: #{highest * found}"
-      break
-    end
-    highest = numbers.pop
-  end
+  puts 'Part 1:'
+  puts find_sum_pair(numbers, TARGET_SUM)
+  puts find_sum_pair(numbers, TARGET_SUM).inject { |acc, num| acc * num }
 end
 
 main
