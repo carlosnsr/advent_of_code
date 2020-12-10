f9c6f5ef8ed7a2e1c6b0df432d816d1904a7f85f
Carlos Rodrigues <carlos@bloom.co>
2020-12-09 23:52:12 -0700

    Used lodash.  Generalised and refactored.

--- 2020/1/js/solution.js
+++ 2020/1/js/solution.js
@@ -1,74 +1,25 @@
 const fs = require('fs')
-
-// Binary search for a value equal to or smaller than the given value
-// LTE match (Less than or equal)
-function bestMatch(arr, find){
-  let start = 0
-  let end   = arr.length -1
-  let mid
-  while (start <= end){
-    mid = Math.ceil((start + end) / 2)
-    // console.log([start, mid, end])
-    if (find == arr[mid]){
-      return mid
-    } else if (find < arr[mid]){
-      end = mid -1
-    } else if (find > arr[mid]){
-      start = mid + 1
-    }
-  }
-  // left edge case for when no value smaller than the given value is found
-  if (mid == 0 && find != arr[mid]){
-    return -1
-  }
-  return mid
-}
-
-// returns the index of the value that matches the value
-// end: size of array
-const binary_search_index = (sorted, value, start, end) => {  // O(log(n))
-  if (end <= start)
-    return -1
-
-  const middle = Math.floor((start + (end - 1)) / 2)
-  const middle_value = sorted[middle]
-  // console.log(`Searching for ${start} ${end} ${middle}: ${middle_value}`)
-  if (middle_value == value) {
-    return middle
-  } else if (value < middle_value ) {
-    return binary_search_index(sorted, value, start, middle)
-  } else {
-    return binary_search_index(sorted, value, middle + 1, end)
-  }
-}
+const _ = require('lodash')
 
 // numbers: sorted array of numbers
-const find_pair = (numbers, target_sum = 2020, start = 0, end = undefined) => {  // O(nlog(n))
-  if (!end) {
-    end = numbers.length
-  }
-  let found = -1  // assume not found
-  for(let i = start; i < end; ++i) {
-    // console.log(`Checking ${i}: ${numbers[i]}`)
-    compare = numbers[i]
-    difference = target_sum - compare
-    // console.log('in find_pair', compare, difference)
-    found = binary_search_index(numbers, difference, i + 1, end)
-    if (found != -1)
-      return [i, found]
+const find_pair = (numbers, target_sum = 2020, start = 0) => {  // O(nlog(n))
+  let mid_point = Math.ceil(target_sum / 2)
+  let stop  = 1 + _.sortedIndex(numbers, mid_point)
+  for(let i = start; i < stop; ++i) {
+    const difference = target_sum - numbers[i]
+    const best_match = _.sortedIndex(numbers, difference)
+    if (numbers[best_match] == difference)
+      return [i, best_match]
   }
   return []
 }
 
 const find_trio = (numbers, target_sum = 2020) => {
-  let found = -1  // assume not found
-  let pair = []
-  for(let i = 0; i < numbers.length; ++i) {
-    compare = numbers[i]
-    difference = target_sum - compare
-    upper_limit = 1 + bestMatch(numbers, difference)
-    pair = find_pair(numbers, difference, i + 1, upper_limit)
-    // console.log('in find_trio:', pair)
+  let mid_point = Math.ceil(target_sum / 2)
+  let stop  = 1 + _.sortedIndex(numbers, mid_point)
+  for(let i = 0; i < stop; ++i) {
+    const difference = target_sum - numbers[i]
+    const pair = find_pair(numbers, difference, i + 1)
     if (pair.length != 0) {
       return [i, ...pair]
     }
@@ -76,42 +27,25 @@ const find_trio = (numbers, target_sum = 2020) => {
   return []
 }
 
-const part_1 = (numbers) => {
-  const answer = find_pair(numbers)
-  answer.forEach( index => console.log(`${index}: ${numbers[index]}`) )
+const get_solution = (part, numbers, fn) => {
+  console.log(`Part ${part}`)
+  const answer = fn(numbers)
+  answer.forEach(index => console.log(`${index}: ${numbers[index]}`))
   const product = answer.reduce((acc, index) => acc * numbers[index], 1)
-  console.log(product)
-}
-
-const part_2 = (numbers) => {
-  const answer = find_trio(numbers)
-  // console.log(answer)
-  answer.forEach( index => console.log(`${index}: ${numbers[index]}`) )
-  const product = answer.reduce((acc, index) => acc * numbers[index], 1)
-  console.log(product)
+  console.log(`Product: ${product}`)
 }
 
 const main = () => {
   // Read in and sort input
-  const file_contents = fs.readFileSync('./input', 'utf8')  // O(n)
+  const file_contents = fs.readFileSync('../puzzle_input', 'utf8')  // O(n)
   const numbers = file_contents.split('\n').map( str => parseInt(str) ) // O(n)
   const ascending_sort = (a, b) => a - b
   const sorted_numbers = numbers.sort(ascending_sort) // O(nlog(n)
 
-  const test_numbers = [
-    299,
-    366,
-    675,
-    979,
-    1456,
-    1721,
-  ]
   // Part 1
-  // part_1(test_numbers)
-  part_1(sorted_numbers)
+  get_solution(1, sorted_numbers, find_pair)
   // Part 2
-  // part_2(test_numbers)
-  part_2(sorted_numbers)
+  get_solution(2, sorted_numbers, find_trio)
 }
 
 main()
