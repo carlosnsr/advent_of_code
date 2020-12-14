1a654ec093fcaa0c95ee4bee3ef4069895d3963f
Carlos Rodrigues <carlos@bloom.co>
2020-12-14 16:49:06 -0700

    Solution to Day 3 Part 2

--- 2020/3/js/solution.js
+++ 2020/3/js/solution.js
@@ -1,21 +1,17 @@
 const fs = require('fs')
 const TREE = '#'
 
-const solver = (map) => {
-  let i = 0
+const count_trees_on_path = (map, right, down) => {
   let position = 0
   let trees = 0
-  while (i < map.length) {
+  for(let i = 0; i < map.length; i += down) {
     const terrain = map[i]
-    // console.log(position, terrain, terrain[position])
     if (terrain[position] == TREE)
       trees++
-    position = (position + 3) % terrain.length
-    ++i
+    position = (position + right) % terrain.length
   }
   return trees
 }
-
 
 const main = () => {
   // Read in map
@@ -36,9 +32,25 @@ const main = () => {
     ".#..#...#.#",
   ]
 
-  // pass map to solver
-  console.log("test: ", solver(test_map))
-  console.log("part 1: ", solver(map))
+  // Part 1
+  // pass map to count_trees_on_path
+  console.log("Test 1: ", count_trees_on_path(test_map, 3, 1))
+  console.log("Part 1: ", count_trees_on_path(map, 3, 1))
+
+  // Part 2
+  const paths = [
+    { right: 1, down: 1 },
+    { right: 3, down: 1 },
+    { right: 5, down: 1 },
+    { right: 7, down: 1 },
+    { right: 1, down: 2 },
+  ]
+  let trees_hit = paths.map(({right, down}) => count_trees_on_path(test_map, right, down))
+  let product = trees_hit.reduce((acc, num) => acc * num, 1)
+  console.log("Test 2: ", product)
+  trees_hit = paths.map(({right, down}) => count_trees_on_path(map, right, down))
+  product = trees_hit.reduce((acc, num) => acc * num, 1)
+  console.log("Part 2: ", product)
 }
 
 main()
