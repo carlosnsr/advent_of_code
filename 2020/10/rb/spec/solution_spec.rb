1ed1935180a12cb5de35d5dfe327f040884f7a71
Carlos Rodrigues <carlos.all.mail@gmail.com>
2021-08-12 01:44:05 -0600

    Renaming to counts is more accurate

--- 2020/10/rb/spec/solution_spec.rb
+++ 2020/10/rb/spec/solution_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe '#load_integers' do
   end
 end
 
-RSpec.describe '#find_voltage_differences' do
+RSpec.describe '#count_voltage_differences' do
   context 'given the test input' do
     TEST_INPUT = [
       28, 33, 18, 42, 31,
@@ -23,23 +23,23 @@ RSpec.describe '#find_voltage_differences' do
 
     it 'the number of 1-volt and 3-volt differences' do
       enumerator = load_integers(TEST_INPUT)
-      expect(find_voltage_differences(enumerator)).to eq([22, 10])
+      expect(count_voltage_differences(enumerator)).to eq([22, 10])
     end
   end
 
   it 'for array, assume 3-difference between the outlet and the final device' do
-    expect(find_voltage_differences([])).to eq([0, 1])
+    expect(count_voltage_differences([])).to eq([0, 1])
   end
 
   it 'compares the first value to 0 (the outlet) and then +3 (the device)' do
-    expect(find_voltage_differences([1])).to eq([1, 1])
-    expect(find_voltage_differences([2])).to eq([0, 1])
-    expect(find_voltage_differences([3])).to eq([0, 2])
+    expect(count_voltage_differences([1])).to eq([1, 1])
+    expect(count_voltage_differences([2])).to eq([0, 1])
+    expect(count_voltage_differences([3])).to eq([0, 2])
   end
 
   it 'can process multi-item sorted arrays' do
-    expect(find_voltage_differences([1, 2, 3])).to eq([3, 1])
-    expect(find_voltage_differences([3, 6, 9])).to eq([0, 4])
+    expect(count_voltage_differences([1, 2, 3])).to eq([3, 1])
+    expect(count_voltage_differences([3, 6, 9])).to eq([0, 4])
   end
 end
 
--- 2020/10/rb/src/solution.rb
+++ 2020/10/rb/src/solution.rb
@@ -7,7 +7,7 @@ end
 #   - each voltage differs by no more than 3 volts from the previous voltage
 #   - excludes the hidden, last voltage of the (last voltage + 3)
 # returns the count of 1-volt and 3-volt differences
-def find_voltage_differences(voltages)
+def count_voltage_differences(voltages)
   last = 0
   result = voltages
     .each_with_object(Array.new(4, 0)) do |voltage, acc|
