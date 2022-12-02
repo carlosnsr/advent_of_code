a77edd12761b6a3c1d699f1912a82875cade39b1
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-01 22:14:23 -0700

    Solves Day 1: Part 1

--- 2022/day_1/src/main.rs
+++ 2022/day_1/src/main.rs
@@ -7,8 +7,20 @@ fn main() {
     let file = File::open(FILENAME).unwrap();
     let reader = BufReader::new(file);
 
-    for (index, line) in reader.lines().enumerate() {
+    let mut sum = 0;
+    let mut largest = 0;
+    for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        println!("{}, {}", index, line);
+        let calories = line.parse::<u32>();
+        match calories {
+            Ok(calories) => sum += calories,
+            Err(_) => {
+                if sum > largest {
+                    largest = sum;
+                }
+                sum = 0
+            }
+        }
     }
+    println!("The most calories carried by any one elf is {}", largest);
 }
