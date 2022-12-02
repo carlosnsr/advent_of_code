78e76f60d7e906ecae8c30c03c59f1c7378c4d71
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-01 21:16:46 -0700

    wip

--- 2020/10/rb/src/main.rb
+++ 2020/10/rb/src/main.rb
@@ -3,3 +3,4 @@ require './src/solution.rb'
 voltages = load_integers(ARGF)
 counts = count_voltage_differences(voltages)
 puts "Part 1: #{counts.reduce { |acc, n| acc * n }}"
+puts "Part 2: #{combinations(differences(voltages))}"
