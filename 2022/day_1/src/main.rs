d5bba16e0ac916dedb69608e59a1e5ba9f2cd1b2
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-01 21:48:55 -0700

    Sets up problem.  Reads in file

---
+++ 2022/day_1/src/main.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const FILENAME: &str = "puzzle_input.txt";
+
+fn main() {
+    let file = File::open(FILENAME).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        println!("{}, {}", index, line);
+    }
+}
