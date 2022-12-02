2bb1c45171474e31322abbd43af764f6b0c6b484
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-01 22:29:03 -0700

    Solves Day 1: Part 2

--- 2022/day_1/src/main.rs
+++ 2022/day_1/src/main.rs
@@ -8,19 +8,27 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut sum = 0;
-    let mut largest = 0;
+    let mut top_four: Vec<u32> = vec![0; 4]; // 4-element zeroed-out vector
     for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
         let calories = line.parse::<u32>();
         match calories {
             Ok(calories) => sum += calories,
             Err(_) => {
-                if sum > largest {
-                    largest = sum;
+                let smallest = top_four[0];
+                if sum > smallest {
+                    top_four[0] = sum;
+                    top_four.sort_unstable();
                 }
                 sum = 0
             }
         }
     }
-    println!("The most calories carried by any one elf is {}", largest);
+
+    top_four.reverse();
+    top_four.pop();
+    println!("The most calories carried by any one elf is {}", top_four[0]);
+
+    let sum_three: u32 = top_four.iter().sum();
+    println!("The sum of the calories carried by the top three elves is {}", sum_three);
 }
