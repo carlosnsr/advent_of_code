2dee2a046f5abe142abb369bd23036293afbd0a0
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-03 04:17:43 -0700

    Solves Day 3: Part 2

--- 2022/day_3/src/main.rs
+++ 2022/day_3/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp::Ordering;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -10,17 +11,31 @@ fn sort(string: &str) -> Vec<char> {
     chars
 }
 
-fn get_dupe_item(left: Vec<char>, right: Vec<char>) -> char {
-    let (mut i, mut j, max) = (0, 0, left.len());
-    while i < max && j < max {
-        match left[i].cmp(&right[j]) {
-            Ordering::Less => i += 1,
-            Ordering::Greater => j += 1,
-            Ordering::Equal => break,
-        }
+fn find_dupe_items(left: &Vec<char>, right: &Vec<char>) -> Vec<char> {
+    let left_set: HashSet<&char> = left.into_iter().collect();
+    let right_set: HashSet<&char> = right.into_iter().collect();
+    let answer: Vec<char> = left_set
+        .intersection(&right_set)
+        .into_iter()
+        .map(|x| **x)
+        .collect();
+    answer
+}
+
+fn get_badge(trio: &Vec<String>) -> char {
+    let trio: Vec<Vec<char>> = trio
+        .iter()
+        .map(|line| sort(line))
+        .collect();
+
+    let intersection = find_dupe_items(&trio[0], &trio[1]);
+    let badges = find_dupe_items(&intersection, &trio[2]);
+    if badges.len() == 1 {
+        badges[0]
+    } else {
+        panic!("Whoops: trio:{:?}  dupes:{:?}", trio, badges)
     }
 
-    left[i]
 }
 
 fn priority(item: char) -> u32 {
@@ -33,20 +48,36 @@ fn priority(item: char) -> u32 {
     score + 1
 }
 
+fn get_rucksack_item_priority(line: &str) -> u32 {
+    let middle = line.len() / 2;
+    let (left, right) = (sort(&line[0..middle]), sort(&line[middle..]));
+    let dupes = find_dupe_items(&left, &right);
+    if dupes.len() == 1 {
+        priority(dupes[0])
+    } else {
+        panic!("Whoops: left:{:?} right:{:?} dupes:{:?}", left, right, dupes)
+    }
+}
+
 fn main() {
     let file = File::open(FILENAME).unwrap();
     let reader = BufReader::new(file);
 
     let mut sum = 0;
+    let mut trio: Vec<String> = vec![];
+    let mut trio_sum = 0;
     for (_index, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        let middle = line.len() / 2;
-        let (left, right) = (sort(&line[0..middle]), sort(&line[middle..]));
-        // println!("{:?} {:?}", left, right);
-        let dupe_item = get_dupe_item(left, right);
-        sum += priority(dupe_item);
+        sum += get_rucksack_item_priority(&line);
+        trio.push(line);
+        if trio.len() == 3 {
+            let badge = get_badge(&trio);
+            trio_sum += priority(badge);
+            trio.clear();
+        }
     }
     println!("Sum of all priorities: {}", sum);
+    println!("Sum of all trio priorities: {}", trio_sum);
 }
 
 #[cfg(test)]
@@ -60,10 +91,10 @@ mod tests {
     }
 
     #[test]
-    fn dupe_item_returns_first_char_that_appears_in_each_string() {
-        let left = vec!['a', 'c', 'e', 'f'];
-        let right = vec!['b', 'd', 'e', 'g'];
-        assert_eq!(get_dupe_item(left, right), 'e');
+    fn find_dupe_items_finds_all_duplicate_items() {
+        let left = vec!['A', 'a', 'c', 'e', 'f'];
+        let right = vec!['b', 'd', 'e', 'f', 'g'];
+        assert_eq!(find_dupe_items(&left, &right), vec!['e', 'f']);
     }
 
     #[test]
@@ -76,5 +107,21 @@ mod tests {
     fn priority_returns_27_to_52_for_uppercase_char() {
         assert_eq!(priority('A'), 27);
         assert_eq!(priority('Z'), 52);
+    }
+
+    #[test]
+    fn get_badge_returns_the_char_common_to_the_trio() {
+        let mut trio = vec![
+            "vJrwpWtwJgWrhcsFMMfFFhFp".into(),
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".into(),
+            "PmmdzqPrVvPwwTWBwg".into(),
+        ];
+        assert_eq!(get_badge(&trio), 'r');
+        trio = vec![
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".into(),
+            "ttgJtRGJQctTZtZT".into(),
+            "CrZsJsPPZsGzwwsLwLmpwMDw".into(),
+        ];
+        assert_eq!(get_badge(&trio), 'Z');
     }
 }
