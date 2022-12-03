f8b71104a376c9acf540bd5aad12d0643ff743f8
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-03 11:53:19 -0700

    Day 3: Refactored.  No need to have sorted Vec<char>

--- 2022/day_3/src/main.rs
+++ 2022/day_3/src/main.rs
@@ -1,61 +1,46 @@
-use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 const FILENAME: &str = "puzzle_input.txt";
 
-fn sort(string: &str) -> Vec<char> {
-    let mut chars: Vec<char> = string.chars().collect();
-    chars.sort();
-    chars
-}
-
-fn find_dupe_items(left: &Vec<char>, right: &Vec<char>) -> Vec<char> {
-    let left_set: HashSet<&char> = left.into_iter().collect();
-    let right_set: HashSet<&char> = right.into_iter().collect();
-    let answer: Vec<char> = left_set
-        .intersection(&right_set)
+fn find_dupe_items(left: &String, right: &String) -> String {
+    let left_set: HashSet<char> = left.chars().collect();
+    let right_set: HashSet<char> = right.chars().collect();
+    left_set.intersection(&right_set)
         .into_iter()
-        .map(|x| **x)
-        .collect();
-    answer
+        .map(|x| *x)
+        .collect::<String>()
 }
 
-fn get_badge(trio: &Vec<String>) -> char {
-    let trio: Vec<Vec<char>> = trio
-        .iter()
-        .map(|line| sort(line))
-        .collect();
-
+fn get_badge(trio: &Vec<String>) -> String {
     let intersection = find_dupe_items(&trio[0], &trio[1]);
     let badges = find_dupe_items(&intersection, &trio[2]);
     if badges.len() == 1 {
-        badges[0]
+        badges
     } else {
         panic!("Whoops: trio:{:?}  dupes:{:?}", trio, badges)
     }
-
 }
 
-fn priority(item: char) -> u32 {
-    let mut score = item as u32 - 'A' as u32;
+fn priority(items: &String) -> u32 {
+    let item = items.chars().next().unwrap();
+    let score = item as u32 - 'A' as u32;
     if score < 26 { // was uppercase, bump up by 27 points
-        score += 26;
+        score + 27
     } else { // was lowercase
-        score = item as u32 - 'a' as u32;
+        item as u32 - 'a' as u32 + 1
     }
-    score + 1
 }
 
 fn get_rucksack_item_priority(line: &str) -> u32 {
     let middle = line.len() / 2;
-    let (left, right) = (sort(&line[0..middle]), sort(&line[middle..]));
-    let dupes = find_dupe_items(&left, &right);
-    if dupes.len() == 1 {
-        priority(dupes[0])
+    let (left, right) = (&line[0..middle].into(), &line[middle..].into());
+    let dupe = find_dupe_items(left, right);
+    if dupe.len() == 1 {
+        priority(&dupe)
     } else {
-        panic!("Whoops: left:{:?} right:{:?} dupes:{:?}", left, right, dupes)
+        panic!("Whoops: left:{:?} right:{:?} dupe:{:?}", left, right, dupe)
     }
 }
 
@@ -72,7 +57,7 @@ fn main() {
         trio.push(line);
         if trio.len() == 3 {
             let badge = get_badge(&trio);
-            trio_sum += priority(badge);
+            trio_sum += priority(&badge);
             trio.clear();
         }
     }
@@ -85,28 +70,23 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sort_sorts_the_characters_in_a_string() {
-        let expected: Vec<char> = "ABCDabcd".chars().collect();
-        assert_eq!(sort("BadCAbDc"), expected);
-    }
-
-    #[test]
     fn find_dupe_items_finds_all_duplicate_items() {
-        let left = vec!['A', 'a', 'c', 'e', 'f'];
-        let right = vec!['b', 'd', 'e', 'f', 'g'];
-        assert_eq!(find_dupe_items(&left, &right), vec!['e', 'f']);
+        let left = "Aacef".into();
+        let right = "bdefg".into();
+        let actual = find_dupe_items(&left, &right);
+        assert!(["ef", "fe"].iter().any(|s| s == &actual.as_str()));
     }
 
     #[test]
     fn priority_returns_1_to_26_for_lowercase_char() {
-        assert_eq!(priority('a'), 1);
-        assert_eq!(priority('z'), 26);
+        assert_eq!(priority(&"a".into()), 1);
+        assert_eq!(priority(&"z".into()), 26);
     }
 
     #[test]
     fn priority_returns_27_to_52_for_uppercase_char() {
-        assert_eq!(priority('A'), 27);
-        assert_eq!(priority('Z'), 52);
+        assert_eq!(priority(&"A".into()), 27);
+        assert_eq!(priority(&"Z".into()), 52);
     }
 
     #[test]
@@ -116,12 +96,12 @@ mod tests {
             "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL".into(),
             "PmmdzqPrVvPwwTWBwg".into(),
         ];
-        assert_eq!(get_badge(&trio), 'r');
+        assert_eq!(get_badge(&trio), "r");
         trio = vec![
             "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn".into(),
             "ttgJtRGJQctTZtZT".into(),
             "CrZsJsPPZsGzwwsLwLmpwMDw".into(),
         ];
-        assert_eq!(get_badge(&trio), 'Z');
+        assert_eq!(get_badge(&trio), "Z");
     }
 }
