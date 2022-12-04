a3b34d51d465ffd1ab5900fcaaf5a5a47acef798
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-04 12:39:36 -0700

    Day 4: Solves Part 1

--- 2022/day_4/src/main.rs
+++ 2022/day_4/src/main.rs
@@ -1,25 +1,28 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::RangeInclusive;
 
 const FILENAME: &str = "input";
 
-type WorkPair = (usize, usize);
+type WorkPair = RangeInclusive<usize>;
 
 #[derive(Debug, PartialEq)]
 struct Work {
-    left: WorkPair,
     right: WorkPair,
+    left: WorkPair,
 }
 
 impl Work {
     fn parse_line(line: String) -> Self {
-        let pairs = line
+        let mut ranges = line
             .split(",")
             .collect::<Vec<&str>>()
             .iter()
             .map(|s| Self::parse_pair(s))
             .collect::<Vec<WorkPair>>();
-        Work { left: pairs[0], right: pairs[1] }
+        let right = ranges.pop().unwrap();
+        let left = ranges.pop().unwrap();
+        Work { left: left, right: right }
     }
 
     fn parse_pair(input: &str) -> WorkPair{
@@ -29,7 +32,16 @@ impl Work {
             .iter()
             .map(|s| s.parse::<usize>().unwrap())
             .collect::<Vec<usize>>();
-        (numbers[0], numbers[1])
+        numbers[0]..=numbers[1]
+    }
+
+    fn has_superset(&self) -> bool {
+        Work::superset(&self.left, &self.right)
+            || Work::superset(&self.right, &self.left)
+    }
+
+    fn superset(left: &WorkPair, right: &WorkPair) -> bool {
+        left.contains(&right.start()) && left.contains(&right.end())
     }
 }
 
@@ -37,16 +49,15 @@ fn main() {
     let file = File::open(FILENAME).unwrap();
     let reader = BufReader::new(file);
 
-    let mut sum = 0;
+    let mut count = 0;
     for (index, line) in reader.lines().enumerate() {
         let work = Work::parse_line(line.unwrap());
-        println!("{:?}", work);
-        if index == 10 {
-          break;
+        if work.has_superset() {
+            count += 1;
         }
     }
 
-    println!("The sum is {}", sum);
+    println!("The count is {}", count);
 }
 
 #[cfg(test)]
@@ -55,13 +66,31 @@ mod tests {
 
     #[test]
     fn work_parse_gives_back_work_struct() {
-        let expected = Work { left: (33, 62), right: (26, 62) };
+        let expected = Work { left: 33..=62, right: 26..=62 };
         assert_eq!(Work::parse_line("33-62,26-62".into()), expected);
     }
 
     #[test]
     fn work_pair_parse_gives_back_work_pair_struct() {
-        let expected: WorkPair = (33, 62);
+        let expected: WorkPair = 33..=62;
         assert_eq!(Work::parse_pair("33-62".into()), expected);
+    }
+
+    #[test]
+    fn superset_returns_true_if_left_contains_right() {
+        let left = 26..=62;
+        let right = 33..=62;
+        assert!(Work::superset(&left, &right));
+        assert!(!Work::superset(&right, &left));
+    }
+
+    #[test]
+    fn has_superset_returns_true_if_either_range_contains_the_other() {
+        let mut input = Work { left: 33..=62, right: 26..=62 };
+        assert!(input.has_superset());
+        input = Work { right: 33..=62, left: 26..=62 };
+        assert!(input.has_superset());
+        let mut input = Work { left: 1..=9, right: 2..=10 };
+        assert!(!input.has_superset());
     }
 }
