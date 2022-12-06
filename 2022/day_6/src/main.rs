c70aeda2fc349128f475d19172eb31d5213c1d99
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-05 22:48:19 -0700

    Day 6: Solves Part 2

--- 2022/day_6/src/main.rs
+++ 2022/day_6/src/main.rs
@@ -10,16 +10,19 @@ fn main() {
 
     for line in reader.lines() {
         let line = line.unwrap();
-        let (index, marker) = find_marker(&line).unwrap();
+        let (index, marker) = find_marker(&line, 4).unwrap();
         println!("The marker is {} at {}", marker, index);
+        let (index, marker) = find_marker(&line, 14).unwrap();
+        println!("The start of message is {} at {}", marker, index);
     }
 }
 
-fn find_marker(line: &String) -> Option<(usize, String)> {
-    for i in 0..(line.len()-4) {
-        let slice = &line[i..i+4];
-        let set = &line[i..i+4].chars().collect::<HashSet<char>>();
-        if set.len() == 4 { return Some((i+3+1, slice.into())) }
+fn find_marker(line: &String, length: usize) -> Option<(usize, String)> {
+    let end = line.len() - length;
+    for i in 0..end {
+        let slice = &line[i..i+length];
+        let set = &line[i..i+length].chars().collect::<HashSet<char>>();
+        if set.len() == length { return Some((i+length, slice.into())) }
     }
 
     None
@@ -32,6 +35,12 @@ mod tests {
     #[test]
     fn find_marker_returns_marker() {
         let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".into();
-        assert_eq!(find_marker(&input), Some((7, "jpqm".into())));
+        assert_eq!(find_marker(&input, 4), Some((7, "jpqm".into())));
+    }
+
+    #[test]
+    fn find_marker_returns_marker_of_length_14() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".into();
+        assert_eq!(find_marker(&input, 14), Some((19, "qmgbljsphdztnv".into())));
     }
 }
