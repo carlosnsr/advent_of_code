bdf265f482fabfc3d2de5ca81e47658572b372e9
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-07 22:17:32 -0700

    Day 8: README and initial setup

---
+++ 2022/day_8/src/main.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const FILENAME: &str = "input";
+
+fn main() {
+    let file = File::open(FILENAME).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut sum = 0;
+    for (_index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+    }
+
+    println!("The sum is {}", sum);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(1, 1);
+    }
+}
