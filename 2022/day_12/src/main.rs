fb48abc1c88c8867c58a286b029effe93dca29f6
Carlos Rodrigues <carlos.all.mail@gmail.com>
2022-12-13 04:14:00 -0700

    wip: hill climb algorithm works for 3x3...

--- 2022/day_12/src/main.rs
+++ 2022/day_12/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -18,36 +19,33 @@ fn main() {
     println!("The grid is {:?}", grid);
 }
 
-#[derive(Clone, Debug, PartialEq)]
-struct Coord {
-    x: usize,
-    y: usize,
-}
-
-impl Coord {
-    fn new(x: usize, y: usize) -> Self {
-        Self{ x, y }
-    }
-}
-
-type Height = usize;
+type Height = char;
 type Heights = Vec<Height>;
 
 #[derive(Debug)]
 struct Grid {
     grid: Vec<Heights>,
-    visited: Vec<Vec<char>>,
 }
 
 impl Grid {
     fn new() -> Self {
-        Self { grid: Vec::new(), visited: Vec::new() }
+        Self { grid: Vec::new() }
     }
 
     fn push(&mut self, line: &String) {
-        let heights = convert_to_heights(&line);
-        self.visited.push(vec!['.'; heights.len()]);
+        let heights = line.chars().collect::<Heights>();
         self.grid.push(heights);
+    }
+
+    fn find(&self, value: Height) -> Option<Point> {
+        for y in 0..self.len_y() {
+            for x in 0..self.len_x() {
+                if self.grid[y][x] == value {
+                    return Some(Point::new(x, y));
+                }
+            }
+        }
+        None
     }
 
     fn len_x(&self) -> usize {
@@ -57,117 +55,93 @@ impl Grid {
     fn len_y(&self) -> usize {
         self.grid.len()
     }
+}
 
-    fn find(&self, target: Height) -> Option<Coord> {
-        for y in 0..self.len_y() {
-            for x in 0..self.len_x() {
-                if self.grid[y][x] == target {
-                    return Some(Coord::new(x, y));
-                }
-            }
-        }
-        None
-    }
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct Point {
+    x: usize,
+    y: usize,
+}
 
-    fn get(&self, target: &Coord) -> Option<Height> {
-        let (x, y) = (target.x, target.y);
-        Some(self.grid[y][x])
-    }
-
-    fn mark(&mut self, target: &Coord, mark: char) {
-        let (x, y) = (target.x, target.y);
-        self.visited[y][x] = mark;
-    }
-
-    fn neighbours(&self, target: &Coord) -> Vec<(char, Coord)> {
-        let (x, y) = (target.x, target.y);
-        let mut neighbours = Vec::new();
-        if x + 1 < self.len_x() {
-            neighbours.push(('>', Coord::new(x + 1, y))); // right
-        }
-        if x > 0 {
-            neighbours.push(('>', Coord::new(x - 1, y))); // left
-        }
-        if y > 0 {
-            neighbours.push(('^', Coord::new(x, y - 1))); // up
-        }
-        if y + 1 < self.len_y() {
-            neighbours.push(('v', Coord::new(x, y + 1))); // down
-        }
-
-        neighbours
-    }
-
-    fn was_visited(&self, target: &Coord) -> bool {
-        let (x, y) = (target.x, target.y);
-        self.visited[y][x] != '.'
+impl Point {
+    fn new(x: usize, y: usize) -> Self {
+        Self{ x, y }
     }
 }
 
-fn convert_to_heights(line: &String) -> Heights {
-    println!("{:?}", line);
-    line
-        .chars()
-        .map(|c| match c {
-            'S' => 0,
-            'E' => 99,
-            _ => c as usize - 'a' as usize + 1
-        })
-        .collect::<Heights>()
+fn climb_to_top(grid: &Grid) -> Option<Vec<Point>> {
+    let start = grid.find('S').unwrap();
+    let end = grid.find('E').unwrap();
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    let mut path = match hill_climb(&grid, &start, &end, &mut visited) {
+        Some(mut path) => {
+            path.push(start.clone());
+            path.reverse();
+            Some(path)
+        },
+        None => None,
+    };
+
+    path
 }
 
-fn climb(grid: &Grid) -> Option<Vec<Coord>> {
-    let start = grid.find(0).unwrap();
-    println!("Found {:?}", start);
-    let end = grid.find(99).unwrap();
-    println!("Found {:?}", end);
-
-    hill_climb(&mut grid, &start, &end)
-}
-
-fn hill_climb(grid: &mut Grid, target: &Coord, end: &Coord) -> Option<Vec<Coord>> {
-    if target == end {
-        Some(vec![target.clone()])
+fn hill_climb(grid: &Grid, current: &Point, end: &Point, visited: &mut HashSet<Point>) -> Option<Vec<Point>> {
+    visited.insert(current.clone());
+    if current == end {
+        return Some(vec![]);
     } else {
-        let neighbours = grid.neighbours(&target);
-        println!("{:?}", neighbours);
-        for (mark, neighbour) in neighbours.iter() {
-            if is_climbable(&grid, target, neighbour) {
-                grid.mark(target, mark.clone());
-                match hill_climb(&mut grid, &neighbour, &end) {
-                    Some(mut path) => {
-                        path.push(neighbour.clone());
-                        return Some(path);
-                    },
-                    None => (),
-                }
+        let neighbours = find_neighbours(grid, current, &visited);
+        for i in 0..neighbours.len() {
+            match hill_climb(&grid, &neighbours[i], &end, visited) {
+                Some(mut path) => {
+                    path.push(neighbours[i].clone());
+                    return Some(path);
+                },
+                None => (),
             }
         }
-
-        None
     }
+
+    None
 }
 
-fn is_climbable(grid: &Grid, source: &Coord, target: &Coord) -> bool {
-    let source_height = grid.get(&source).unwrap() as isize;
-    let target_height = grid.get(&target).unwrap() as isize;
-    println!("{:?} {:?}", source_height, target_height);
+fn find_neighbours(grid: &Grid, center: &Point, visited: &HashSet<Point>) -> Vec<Point> {
+    let (x, y) = (center.x, center.y);
+    let mut neighbours = Vec::new();
+    if x + 1 < grid.len_x() {
+        let neighbour = Point::new(x + 1, y);
+        if !visited.contains(&neighbour) {
+            neighbours.push(neighbour); // right
+        }
+    }
+    if x > 0 {
+        let neighbour = Point::new(x - 1, y);
+        if !visited.contains(&neighbour) {
+            neighbours.push(neighbour); // left
+        }
+    }
+    if y > 0 {
+        let neighbour = Point::new(x, y - 1);
+        if !visited.contains(&neighbour) {
+            neighbours.push(neighbour); // up
+        }
+    }
+    if y + 1 < grid.len_y() {
+        let neighbour = Point::new(x, y + 1);
+        if !visited.contains(&neighbour) {
+            neighbours.push(neighbour); // down
+        }
+    }
 
-    target_height == 99 || (source_height - target_height).abs() <= 1
+    neighbours
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn input_grid() -> Grid {
-        let lines = vec![
-            "Sabqponm".into(),
-            "abcryxxl".into(),
-            "accszExk".into(),
-            "acctuvwj".into(),
-            "abdefghi".into(),
-        ];
+    fn make_grid(lines: &Vec<String>) -> Grid {
         let mut grid = Grid::new();
         for line in lines.iter() {
             grid.push(&line);
@@ -175,6 +149,17 @@ mod tests {
         grid
     }
 
+    fn example_input() -> Vec<String> {
+        vec![
+            "Sabqponm".into(),
+            "abcryxxl".into(),
+            "accszExk".into(),
+            "acctuvwj".into(),
+            "abdefghi".into(),
+        ]
+    }
+
+    /*
     #[ignore]
     #[test]
     fn test_hill_climb() {
@@ -191,15 +176,36 @@ mod tests {
         assert!(false);
         // assert_eq!(climb(input_grid), expected);
     }
+    */
 
     #[test]
     fn test_little_hill_climbs() {
-        let mut grid = Grid::new();
-        grid.push(&"Sa".into());
-        grid.push(&"bE".into());
-        println!("{:?}", grid);
-        climb(&grid);
-        println!("{:?}", grid);
-        assert!(false);
+        let mut input = vec![
+            "Sa".into(),
+            "bE".into(),
+        ];
+        let mut grid = make_grid(&input);
+        let mut path = climb_to_top(&grid);
+        // println!("{:?}", path);
+        assert_eq!(path, Some(vec![Point::new(0, 0), Point::new(1, 0), Point::new(1, 1)]));
+
+        input = vec![
+            "Sac".into(),
+            "bbd".into(),
+            "dcE".into(),
+        ];
+        grid = make_grid(&input);
+        path = climb_to_top(&grid);
+        // println!("{:?}", path);
+        assert_eq!(
+            path,
+            Some(vec![
+                Point::new(0, 0),
+                Point::new(1, 0),
+                Point::new(1, 1),
+                Point::new(1, 2),
+                Point::new(2, 2),
+            ])
+        );
     }
 }
