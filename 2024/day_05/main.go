1c8560f558407ea126aa695035065705486b9a56
Carlos Rodrigues <carlos.rodrigues@ibotta.com>
2024-12-09 19:45:37 -0700

    Reads in the rules

--- 2024/day_05/main.go
+++ 2024/day_05/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
   "fmt"
+  "regexp"
 )
 
 func main() {
-  fmt.Println("Hello, World!")
+  re, err := regexp.Compile(`(\d+)\|(\d+)`)
+  check(err)
+
+  // read in the rules
+  rules := make(map[int](map[int]bool))
+  scanner := open_file("./example.input")
+  for scanner.Scan() {
+    parts := re.FindStringSubmatch(scanner.Text())
+    if len(parts) == 0 {
+      break
+    }
+    pre := to_i(parts[1])
+    post := to_i(parts[2])
+    if _, ok := rules[pre]; !ok {
+      rules[pre] = make(map[int]bool)
+    }
+    rules[pre][post] = true
+  }
+  fmt.Println(rules)
 }
