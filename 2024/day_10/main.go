2b7f4fe358625f8b76d1c037a8f1f6912793c690
Carlos Rodrigues <carlos.rodrigues@ibotta.com>
2024-12-09 23:18:00 -0700

    Reads in the grid. Scans for zeroes

--- 2024/day_10/main.go
+++ 2024/day_10/main.go
@@ -5,5 +5,30 @@ import (
 )
 
 func main() {
-  fmt.Println("Hello, World!")
+  const MAX_SIZE = 60
+  grid := make([]string, MAX_SIZE)
+
+  // read in the grid
+  lines := 0
+  scanner := open_file("./example.input")
+  for scanner.Scan() {
+    grid[lines] = scanner.Text()
+    lines++
+  }
+  fmt.Println(grid)
+
+  // find each zero, and walk to as many nines as possible
+  for y := 0; y < lines; y++ {
+    row := grid[y]
+    for x, c := range row {
+      if c == '0' {
+        fmt.Println(row)
+        walk(y, x, grid)
+      }
+    }
+  }
+}
+
+func walk(y, x int, grid []string) {
+  fmt.Printf("Walking from (%d, %d)\n", y, x)
 }
